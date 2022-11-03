994561b775799040eb5906ad5d80382d80579823
Jakub Łanecki <j.lanecki@avsystem.com>
2022-11-03 10:27:28 +0100

    Move towards stateful decoding over multiple decode calls.

--- rds/block_sync.py
+++ rds/block_sync.py
@@ -38,15 +38,20 @@ def lookup_syndrome(syndrome):
             return word_symbol
     return None
 
-
-offset_word_predecessors = {
-    'A': {'D'},
-    'B': {'A'},
-    'C': {'B'},
-    'Cp': {'B'},
-    'D': {'C', 'Cp'}
+word_to_offset = {
+    'A': 0, 'B': 26, 'C': 52, 'Cp': 52, 'D': 78
+}
+offset_to_words = {
+    0: {'A'}, 26: {'B'}, 52: {'C', 'Cp'}, 78: {'D'}
 }
 
+def find_predecessors(word_symbol):
+    offset = word_to_offset[word_symbol]
+    return offset_to_words[(offset - 26) % 104]
+
+def find_next_group_shift(word_symbol):
+    offset = word_to_offset[word_symbol]
+    return 104 - offset
 
 # TODO: probably turn it into RadioText encoding?
 # produce bit sequence of subsequent 104-bit blocks
@@ -66,32 +71,57 @@ def errors(bits):
 
 class Decoder:
     def __init__(self):
+        # synchronization
         self.in_sync = False
         self.current_offset = 0
         self.prev_offsets_size = 27
         self.prev_offset_words = [None] * self.prev_offsets_size
+
+        # shift
+        self.decoding_start = 0
+
+        # decoding
         self.errors_per_block = 0
+        self.prev_bit_arr = np.array([])
 
     def decode(self, bit_arr):
-        # find 2 syndromes corresponding to known offset words in correct order
-        i = 0
-        while i < len(bit_arr) - 26 and not self.in_sync:
-            syndrome = np.matmul(bit_arr[i:i + 26], pc_matrix) % 2
-            offset_word = lookup_syndrome(syndrome)
-            self.prev_offset_words[self.current_offset] = offset_word
+        # steps:
+        #   1. SYNC: find 2 consecutive syndromes corresponding to known offset words in correct order
+        #   2. SHIFT: shift to beginning of the next group (might not be in bit_arr!)
+        #   3. DECODE: decode as many groups as can fit bit_arr (accumulate decoding errors)
+        #   4. SAVE: if non-decoded bits of bit_arr remain - store them for the next decoding round
+        if not self.in_sync:
+            # synchronize
+            i = 0
+            while i < len(bit_arr) - 26 and not self.in_sync:
+                syndrome = np.matmul(bit_arr[i:i + 26], pc_matrix) % 2
+                offset_word = lookup_syndrome(syndrome)
+                self.prev_offset_words[self.current_offset] = offset_word
 
-            if offset_word:
-                # we check if previous offset words matched this one
-                correct_predecessors = offset_word_predecessors[offset_word]
-                if self.prev_offset_words[(self.current_offset - 26) % self.prev_offsets_size] in correct_predecessors:
-                    self.in_sync = True
-                    print('IN SYNC!')
-                    break
+                if offset_word:
+                    # we check if previous offset words matched this one
+                    correct_predecessors = find_predecessors(offset_word)
+                    if self.prev_offset_words[(self.current_offset - 26) % self.prev_offsets_size] in correct_predecessors:
+                        self.in_sync = True
+                        break
 
-            self.current_offset = (self.current_offset + 1) % 27
-            i += 1
+                self.current_offset = (self.current_offset + 1) % self.prev_offsets_size
+                i += 1
+
+            # find group start
+            if self.in_sync:
+                # current and (current - 26) are correct offset words - start looking from the earlier one
+                shift = find_next_group_shift(self.prev_offset_words[(self.current_offset - 26) % self.prev_offsets_size])
+                if i + shift >= len(bit_arr):
+                    # next call to decode method will do the decoding, this one has no data yet
+                    self.decoding_start = i + shift - len(bit_arr)
+                    return
+                else:
+                    self.decoding_start = i + shift
 
         # now we can start decoding the data and accumulating potential errors
+
+
         return self.current_offset
 
 
